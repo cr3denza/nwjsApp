c7e7691c5e57b81d9e02508fa3439f1ffde76d4d
cr3denza <jingkoo87@gmail.com>
2016-02-26 23:17:58 +0900

    first commit

---
+++ app/main.js
@@ -0,0 +1,15 @@
+$( function () {
+});
+
+var array = ['one', 'two', 'three'];
+
+var objec = {
+    a : 'IamA',
+    b : 'IamB',
+    c : 'IamC',
+    d : function (str) {
+        alert(str);
+    }
+};
+window.open('http://youtube.com', 'naver')
+window.open('http://youtube.com', 'naver')
